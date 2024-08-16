bdff6d21a1ba52d42071595be14e313d0d62d395
TimothyRedPanda <timothy@chewedleashphotography.com>
2024-08-16 12:57:37 +0100

    First Deploy

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className="w-screen grid place-content-center h-screen">
+			<p className="font-bold">Joe's Average Joes</p>
+		</div>
+	);
+}
+
+export default App;
