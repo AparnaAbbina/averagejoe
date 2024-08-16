d7a3c91a2836c5c9e801b078be39478567bd4985
AparnaAbbina <149721457+AparnaAbbina@users.noreply.github.com>
2024-08-16 17:03:09 +0100

    Working on displaying the conversation

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import askQuestion from './lib/fetch';
 import {useState} from "react";
 
 function App() {
-    const [inputVal,setInputVal] = useState('Hello World!')
+    const [inputVal,setInputVal] = useState('Hello World!');
+	const [memory, setMemory] = useState('');
+
+	function dataset(){
+		askQuestion(inputVal);
+        setMemory(inputVal);
+
+	}
+
 		return (
 		<main className="w-full text-center m-10">
 			<div className="w-full flex p-10 m-5"><h1 className="font-extrabold text-left text-4xl text-blue-800">Joe's Average Joes</h1></div>
             <div><h2 className="font-bold">"Welcome to Joe's Average Joes! Get career advice and explore job options in this friendly chat for students aged 16-18. Discover your future today!"</h2></div>
-			<p>Answer Field</p>
+			<p>{memory}</p>
 			<input type="text"  value={inputVal} onChange={e => setInputVal(e.target.value)} />
-			<button onClick={()=>askQuestion(inputVal)}>Submit</button>
+			<button onClick={()=>dataset()}>Submit</button>
 		</main>
 	);
 }
