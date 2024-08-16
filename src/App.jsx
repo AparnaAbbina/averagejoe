abe6f8f85b427d2398e1cd3906c450cfe9fce55f
AparnaAbbina <149721457+AparnaAbbina@users.noreply.github.com>
2024-08-16 16:54:22 +0100

    Retrieving response based on the user Input

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import askQuestion from './lib/fetch';
+import {useState} from "react";
 
 function App() {
-	askQuestion('Are you working? Im looking for work at the moment. Any suggestions for someone who has completed a coding bootcamp?');
+    const [inputVal,setInputVal] = useState('Hello World!')
 		return (
 		<main className="w-full text-center m-10">
 			<div className="w-full flex p-10 m-5"><h1 className="font-extrabold text-left text-4xl text-blue-800">Joe's Average Joes</h1></div>
             <div><h2 className="font-bold">"Welcome to Joe's Average Joes! Get career advice and explore job options in this friendly chat for students aged 16-18. Discover your future today!"</h2></div>
 			<p>Answer Field</p>
-			<input type="text" defaultValue="Ask a question..." />
-			<button type="button">Submit</button>
+			<input type="text"  value={inputVal} onChange={e => setInputVal(e.target.value)} />
+			<button onClick={()=>askQuestion(inputVal)}>Submit</button>
 		</main>
 	);
 }
