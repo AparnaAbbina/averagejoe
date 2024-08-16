3b0a5b0d1e6568b88fc6753169ab51465c85ebca
AparnaAbbina <149721457+AparnaAbbina@users.noreply.github.com>
2024-08-16 15:11:40 +0100

    Added logo and description of the service

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 function App() {
 	return (
-		<main className="gap-4 w-screen grid place-content-center h-screen">
-			<h1 className="font-bold">Joe's Average Joes</h1>
+		<main className="w-full text-center m-10">
+			<div className="w-full flex p-10 m-5"><h1 className="font-extrabold text-left text-4xl text-blue-800">Joe's Average Joes</h1></div>
+            <div><h2 className="font-bold">"Welcome to Joe's Average Joes! Get career advice and explore job options in this friendly chat for students aged 16-18. Discover your future today!"</h2></div>
 			<p>Answer Field</p>
 			<input type="text" defaultValue="Ask a question..." />
 			<button type="button">Submit</button>
